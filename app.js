9e0e884b24634db852e3dcc052b8c6937b07da6b
Alex Rickey Winland <alex.winland@hotmail.com>
2024-09-05 12:26:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dinossauro of dinossauros) {
+        // Cria um novo elemento HTML para cada resultado
+        resultados += `
+                <div class="item-resultado">
+                    <h2>
+                        <a href="#" target="_blank">${dinossauro.nome}</a>
+                    </h2>
+                    <p class="descricao-meta">Alimentação: ${dinossauro.alimentacao}</p>
+                    <p class="descricao-meta">Habitat: ${dinossauro.habitat}</p>
+                    <p class="descricao-meta">${dinossauro.descricao}</p>
+                    <a href="${dinossauro.link}" target="_blank">Mais informações</a>
+                </div>
+        `;
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,37 @@
+let dinossauros = [
+    {
+        nome: "Tiranossauro Rex",
+        descricao: "O Tiranossauro Rex era um dos maiores carnívoros terrestres que já existiu. Tinha dentes afiados e uma mordida poderosa.",
+        habitat: "América do Norte",
+        alimentacao: "Carnívoro",
+        link: "https://pt.wikipedia.org/wiki/Tiranossauro_rex"
+    },
+    {
+        nome: "Braquiossauro",
+        descricao: "O Braquiossauro era um dinossauro herbívoro de pescoço longo e cauda curta. Era um dos maiores animais terrestres que já viveram.",
+        habitat: "América do Norte",
+        alimentacao: "Herbívoro",
+        link: "https://pt.wikipedia.org/wiki/Braquiossauro"
+    },
+    {
+        nome: "Velociraptor",
+        descricao: "O Velociraptor era um dinossauro carnívoro bípede e ágil. Tinha garras afiadas nas patas traseiras.",
+        habitat: "Ásia",
+        alimentacao: "Carnívoro",
+        link: "https://pt.wikipedia.org/wiki/Velociraptor"
+    },
+    {
+        nome: "Tricerátops",
+        descricao: "O Tricerátops era um dinossauro herbívoro quadrúpede com três chifres no focinho e uma grande gola óssea no pescoço.",
+        habitat: "América do Norte",
+        alimentacao: "Herbívoro",
+        link: "https://pt.wikipedia.org/wiki/Tricerátops"
+    },
+    {
+        nome: "Estegossauro",
+        descricao: "O Estegossauro era um dinossauro herbívoro quadrúpede com placas ósseas nas costas e pontas afiadas na cauda.",
+        habitat: "América do Norte",
+        alimentacao: "Herbívoro",
+        link: "https://pt.wikipedia.org/wiki/Estegossauro"
+    }
+];
