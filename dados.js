60c9092df3e5663991d7ec1a68dffbbe02cedbb2
Alex Rickey Winland <alex.winland@hotmail.com>
2024-09-06 17:13:26 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -1,47 +1,62 @@
-let dinossauros = [
+let dados = [
   {
-    nome: "Tiranossauro Rex",
-    imagem: "tiranossauro-rex.png",
+    titulo: "Tiranossauro Rex",
+    img: "imagens/tiranossauro-rex.png",
     descricao:
       "O Tiranossauro Rex era um dos maiores carnívoros terrestres que já existiu. Tinha dentes afiados e uma mordida poderosa.",
     habitat: "América do Norte",
     alimentacao: "Carnívoro",
     link: "https://pt.wikipedia.org/wiki/Tiranossauro_rex",
+    tags: "t rex trex tiranossauro carnivoro carne america do norte",
   },
   {
-    nome: "Braquiossauro",
-    imagem: "braquiossauro.png",
+    titulo: "Braquiossauro",
+    img: "imagens/braquiossauro.png",
     descricao:
       "O Braquiossauro era um dinossauro herbívoro de pescoço longo e cauda curta. Era um dos maiores animais terrestres que já viveram.",
     habitat: "América do Norte",
     alimentacao: "Herbívoro",
     link: "https://pt.wikipedia.org/wiki/Braquiossauro",
+    tags: "braquiossauro herbivoro planta america do norte",
   },
   {
-    nome: "Velociraptor",
-    imagem: "velociraptor.png",
+    titulo: "Velociraptor",
+    img: "imagens/velociraptor.png",
     descricao:
       "O Velociraptor era um dinossauro carnívoro bípede e ágil. Tinha garras afiadas nas patas traseiras.",
     habitat: "Ásia",
     alimentacao: "Carnívoro",
     link: "https://pt.wikipedia.org/wiki/Velociraptor",
+    tags: "velociraptor carnivoro carne asia",
   },
   {
-    nome: "Tricerátops",
-    imagem: "triceratops.png",
+    titulo: "Tricerátops",
+    img: "imagens/triceratops.png",
     descricao:
       "O Tricerátops era um dinossauro herbívoro quadrúpede com três chifres no focinho e uma grande gola óssea no pescoço.",
     habitat: "América do Norte",
     alimentacao: "Herbívoro",
     link: "https://pt.wikipedia.org/wiki/Tricerátops",
+    tags: "triceratops herbivoro planta america do norte",
   },
   {
-    nome: "Estegossauro",
-    imagem: "estegossauro.png",
+    titulo: "Pterodáctilo",
+    img: "imagens/pterodactilo.png",
+    descricao:
+      "O Pterodáctilo era um réptil voador carnívoro. Tinha uma envergadura de asas considerável e um bico afiado.",
+    habitat: "África e Europa",
+    alimentacao: "Carnívoro",
+    link: "https://pt.wikipedia.org/wiki/Pterod%C3%A1ctilo",
+    tags: "pterodactilo voador carnivoro africa europa",
+  },
+  {
+    titulo: "Estegossauro",
+    img: "imagens/estegossauro.png",
     descricao:
       "O Estegossauro era um dinossauro herbívoro quadrúpede com placas ósseas nas costas e pontas afiadas na cauda.",
     habitat: "América do Norte",
     alimentacao: "Herbívoro",
     link: "https://pt.wikipedia.org/wiki/Estegossauro",
+    tags: "estegossauro herbivoro planta america do norte",
   },
 ];
